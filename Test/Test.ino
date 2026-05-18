24e0ca42946c10018f56fd3d5c1f8d40928545dc
Asfand Yar Ali Shah <151632937+asfandyaralishah112@users.noreply.github.com>
2026-05-18 07:41:18 +0500

    Added old schematics to Repo.
    * Added V1.0.xxx schematics (EasyEDA)

--- Test/Test.ino
+++ Test/Test.ino
@@ -1,6 +1,7 @@
 #define SENSOR1_PIN 2
 #define SENSOR2_PIN 3
 #define PIR_PIN     5
+#define PIR_PIN1     6
 
 void setup() {
 
@@ -9,6 +10,7 @@ void setup() {
     analogReadResolution(12); // 0 - 4095
 
     pinMode(PIR_PIN, INPUT);
+    pinMode(PIR_PIN1, INPUT);
 
     Serial.println("ADC + PIR Monitor Started");
 }
@@ -19,14 +21,22 @@ void loop() {
     int sensor2 = analogRead(SENSOR2_PIN);
 
     int pirState = digitalRead(PIR_PIN);
+    int pir1State = digitalRead(PIR_PIN1);
 
-    Serial.print("GPIO2: ");
-    Serial.print(sensor1);
+    // Serial.print("GPIO2: ");
+    // Serial.print(sensor1);
 
-    Serial.print("    GPIO3: ");
-    Serial.print(sensor2);
+    // Serial.print("    GPIO3: ");
+    // Serial.print(sensor2);
 
     Serial.print("    PIR(GPIO5): ");
+
+    if (pirState) {
+        Serial.println("MOTION");
+    } else {
+        Serial.println("NO MOTION");
+    }
+    Serial.print("    PIR(GPIO6): ");
 
     if (pirState) {
         Serial.println("MOTION");
